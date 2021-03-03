9c6d97e4581b6d96802431ec859d5514a368661c
Owusu <adjeibohyen@hotmail.co.uk>
2021-03-03 14:30:06 +0000

    initial commit

---
+++ service_000_gcloud/start-development.js
@@ -0,0 +1,18 @@
+const { spawn } = require("child_process");
+const path = require("path");
+
+const dockerComposeFile = path.join(
+  __dirname,
+  "../project_base/docker-handlers/compose-database.yml"
+);
+const mysqlContainer = spawn(
+  "docker-compose",
+  ["-f", dockerComposeFile, "up"],
+  {
+    stdio: [process.stdin, process.stdout, process.stderr],
+  }
+);
+
+mysqlContainer.on("close", (code) => {
+  console.log("Development server exited with code", code);
+});

e39c6be2f2e760529d13ec11af21d324fc4504aa
Owusu <adjeibohyen@hotmail.co.uk>
2021-03-04 13:34:00 +0000

    initial commit

--- service_000_gcloud/start-development.js
+++ service_000_gcloud/start-development.js
@@ -5,14 +5,17 @@ const dockerComposeFile = path.join(
   __dirname,
   "../project_base/docker-handlers/compose-gcloud.yml"
 );
-const mysqlContainer = spawn(
+
+const gcloudContainer = spawn(
   "docker-compose",
-  ["-f", dockerComposeFile, "up"],
+  ["-f", dockerComposeFile, "run", "gcloud", "sh"],
   {
     stdio: [process.stdin, process.stdout, process.stderr],
   }
 );
 
-mysqlContainer.on("close", (code) => {
+gcloudContainer.on("close", (code) => {
   console.log("Development server exited with code", code);
 });
+
+// "start-gcloud": "docker-compose -f ./project_base/docker-handlers/compose-gcloud.yml run gcloud sh",

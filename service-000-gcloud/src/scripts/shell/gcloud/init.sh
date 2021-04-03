14e0ddd40a1fdfe6b8038fe87ccbcf4d28f227d4
Owusu <adjeibohyen@hotmail.co.uk>
2021-04-03 08:12:57 +0100

    initial commit

--- service-000-gcloud/src/scripts/shell/gcloud/init.sh
+++ service-000-gcloud/src/scripts/shell/gcloud/init.sh
@@ -7,9 +7,9 @@ parse_general_flags "$@"
 
 PROJECT_ZONE=${PROJECT_ZONE:-"europe-west2-b"}
 PROJECT_REGION=${PROJECT_REGION:-"europe-west2"}
-PROJECT_ID=${PROJECT_ID:-$(jq -r '.project_id' < "${GCLOUD_SECRETS}"/gcloud-key.json)}
+PROJECT_ID=${PROJECT_ID:-$(jq -r '.project_id' <"${GCLOUD_SECRETS}"/gcloud-key.json)}
 
-gcloud auth activate-service-account  --key-file="${GCLOUD_SECRETS}"/gcloud-key.json
+gcloud auth activate-service-account --key-file="${GCLOUD_SECRETS}"/gcloud-key.json
 
 gcloud config set disable_prompts true
 gcloud config set project "$PROJECT_ID"
@@ -22,4 +22,4 @@ gcloud services enable container.googleapis.com
 gcloud services enable cloudresourcemanager.googleapis.com
 
 gcloud config configurations list
-
+gcloud container clusters list
